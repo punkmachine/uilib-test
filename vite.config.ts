10ba4a422c1515c850e718fb4b4da51d4c4ace6e
Алекс Рассудихин <76869388+punkmachine@users.noreply.github.com>
2024-03-09 06:26:58 +0600

    add: types in library

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,15 @@
 import { defineConfig } from 'vite';
 import path from 'path';
 import vue from '@vitejs/plugin-vue';
+import dts from "vite-plugin-dts";
 
 export default defineConfig({
-  plugins: [vue()],
+  plugins: [
+    vue(),
+    dts({
+      insertTypesEntry: true,
+    }),
+  ],
   build: {
     lib: {
       entry: path.resolve(__dirname, 'src/main.ts'),
